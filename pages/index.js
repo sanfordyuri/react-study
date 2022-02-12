f75521c8c50e1060bc61b31adf9e06405ccb6d89
Yuri <sanfordyuri@gmail.com>
2022-02-12 15:50:03 -0300

    Criação do projeto/criação do contador

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { useReducer, useState } from 'react';
+
+function Contador() {
+
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    ) 
+
+}
+
+function Home() {
+    return <Contador />
+}
+
+export default Home
