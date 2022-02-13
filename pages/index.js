0ada02b5e92b5fcd3456805b7df1ece059344d8c
Yuri <sanfordyuri@gmail.com>
2022-02-13 11:44:16 -0300

    Adicionando Componente personalizado/Criptografia

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,40 @@ function AllFirstUpperCase(props) {
     return <div>{value}</div>
 }
 
+function Criptografar(props) {
+    const texto = props.texto;
+    let text_replaced = texto.replaceAll(' ','@');
+    let tamanho = text_replaced.length;
+    if((tamanho % 8) != 0) {
+       let restante = (8 - (tamanho % 8));
+       for(var i=0; i<restante; i++) {
+        text_replaced = text_replaced + '@';
+       }
+    } 
+    tamanho = text_replaced.length;
+    let c = 0;
+    let palavra = '';
+    var palavras = [];
+    for(var j=0; j<tamanho; j++) {
+        c++;
+        palavra += text_replaced.charAt(j);
+        if(c == 8) {
+            palavras.push(palavra);
+            palavra = '';
+            c = 0;
+        }
+    }
+    let palavraCriptografada = '';
+    for(var k=0; k<8; k++) {
+        for(var vezes = 0; vezes < tamanho/8; vezes++) {
+            palavraCriptografada += palavras[vezes].charAt(k);
+        }
+    }
+
+
+    return <div>{palavraCriptografada}</div>
+}
+
 function Contador() {
 
     const [contador,setContador] = useState(1);
@@ -36,6 +70,7 @@ function Contador() {
             <div>
                 <CapsLock texto="Quero que me coloque em maiusculo"></CapsLock>
                 <AllFirstUpperCase texto="toda primeira letra tem que ser maiuscula"></AllFirstUpperCase>
+                <Criptografar texto="Um exemplo de Entrada"></Criptografar>
             </div>
             <div>{contador}</div>
             <button onClick={adicionarContador}>Adicionar</button>
