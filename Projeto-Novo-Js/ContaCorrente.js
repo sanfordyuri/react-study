2c011503121db1e10b238e3745f5d865c3120ce7
Yuri <sanfordyuri@gmail.com>
2022-02-17 21:05:39 -0300

    Adicionando Getters e Setters na minha classe ContaCorrente.js

--- Projeto-Novo-Js/ContaCorrente.js
+++ Projeto-Novo-Js/ContaCorrente.js
@@ -1,6 +1,19 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
     #saldo = 0;
+    #cliente;
+
+    set cliente(novoValor) {
+        if(novoValor instanceof Cliente) {
+            this.#cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this.#cliente;
+    }
 
     sacar(valor) {
         if(valor > 0) {
@@ -18,5 +31,14 @@ export class ContaCorrente {
 
     mostrarSaldo() {
         return this.#saldo;
+    }
+
+    transferir(valor, conta) {
+        if(valor <= this.#saldo) {
+            const sacar = this.sacar(valor);
+            conta.depositar(sacar);
+        } else {
+            return false
+        }
     }
 }
--- Projeto-Novo-Js/index.js
+++ Projeto-Novo-Js/index.js
@@ -3,6 +3,12 @@ import {ContaCorrente} from "./ContaCorrente.js"
 
 const cc = new ContaCorrente();
 cc.depositar(100);
-console.log(cc.mostrarSaldo());
+cc.agencia = "001";
+const cliente1 = new Cliente();
+cliente1.cpf = "6210374933"
+cliente1.nome = "Joao"
+cc.cliente = cliente1;
+console.log(cc.cliente);
+
 
 
