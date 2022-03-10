19e8e705bee3af3c1fe18b2ecfd2cf345ce25594
Yuri <sanfordyuri@gmail.com>
2022-03-10 19:22:28 -0300

    Subindo Código Aula Youtube

---
+++ buscarcepapp/src/App.js
@@ -0,0 +1,14 @@
+import BuscarCep from "./Components/BuscarCep";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esse é o meu Site React</h1>
+      <BuscarCep />
+    </div>
+  );
+}
+
+// [BuscarCep] => Receba um CEP em um Input e Exiba Logradouro, Bairro e Localidade/Cidade em Paragrafos.
+
+export default App;
---
+++ buscarcepapp/src/Components/BuscarCep.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class BuscarCep extends Component {
+
+    constructor() {
+        super();
+        this.cep = '';
+
+        this.state = ({
+            logradouro: '',
+            bairro: '',
+            cidade: ''
+        });
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBtnClicked = this.handleBtnClicked.bind(this);
+    }
+
+  handleInputChange(e) {
+    this.cep = e.target.value;
+  }    
+
+  async handleBtnClicked(e) {
+    const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+    const data = await fetch(url);
+    const result = await data.json();
+    
+    this.setState({
+        logradouro: result.logradouro,
+        bairro: result.bairro,
+        cidade: result.localidade
+    });
+
+  }
+
+  render() {
+    return (
+      <center>
+          <input 
+          onChange={this.handleInputChange}
+          placeholder='Insira um CEP'/>
+          <div/>
+          <p>Logradouro: {this.state.logradouro}</p>
+          <div/>
+          <p>Bairro: {this.state.bairro}</p>
+          <div/>
+          <p>Cidade: {this.state.cidade}</p>
+          <div/>
+          <button onClick={this.handleBtnClicked}>Pesquisar</button>
+      </center>
+    )
+  }
+}
